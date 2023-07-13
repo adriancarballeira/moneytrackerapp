e6ecc9e7f80bf929ad3827f34494186150562695
agcarba <agcarba@umich.edu>
2023-07-13 10:57:46 -0700

    Add Chase-Prime logic, update Barclays logic

--- Chase-Prime.py
+++ Chase-Prime.py
@@ -1,42 +1,44 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import desc, abs, col, round, expr 
+import pyspark.sql.functions as f
 
 if __name__ == "__main__":
     """
-        Usage:  Reads in .csv of transactions downloaded from Barclays (credit card purchases).
+        Usage:  Reads in .csv of transactions downloaded from Chase Prime (credit card purchases).
                 Analyzes how many dollars are spent where.
                 Returns summary to user.
 
                 To execute:
 ./bin/spark-submit \
-../Git/moneytrackerapp/CC-statement-stats.py
+../Git/moneytrackerapp/Chase-Prime.py
 
     """
 
     spark = SparkSession\
         .builder\
-        .appName("Barclays")\
+        .appName("Chase-Prime")\
         .getOrCreate()
 
     ccStatement = spark\
         .read\
         .option("inferSchema", "true")\
         .option("header", "true")\
-        .csv("./data/Barclays/CreditCard_20230101_20230711.csv")
+        .csv("./data/cc-transactions/Chase7437_Activity20210713_20230713_20230713.CSV")
 
     ccStatement.createOrReplaceTempView("ccStatement")
 
     ccStCount = ccStatement.count()
 
     ccTotals = ccStatement\
-        .groupBy("Description")\
+        .withColumn("transaction_date", f.to_date("Transaction Date", "MM/dd/yyyy"))\
+        .withColumn("transaction_month", f.trunc("transaction_date", "month"))\
+        .groupBy("transaction_month")\
         .sum("amount")\
         .withColumnRenamed("sum(amount)", "amount_total")\
         .orderBy("amount_total")
 
     ccTotals = ccTotals\
-        .withColumn("amount_total", round(ccTotals.amount_total, 2))
+        .withColumn("amount_total", f.round(ccTotals.amount_total, 2))
 
     ccTotals.show()
 
